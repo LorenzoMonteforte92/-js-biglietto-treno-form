bfbb36052696da1930df242bc95be454c464eb62
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-03-06 16:32:10 +0100

    feat: add calc button action

--- JS/script.js
+++ JS/script.js
@@ -2,17 +2,45 @@
 const calcButton = document.querySelector(`#calc-button`);
 calcButton.addEventListener(`click`, function() {
     // al click:
-        // 1. si trasformano i valori in variabili
+    // 1. si trasformano i valori in variabili
+        // dati numerici convertiti da stringa a numero
         const userKm = parseInt(document.querySelector('#input-km').value);
         const userAge = parseInt(document.querySelector(`#input-age`).value);
         console.log(userKm)
         console.log(userAge)
+    // 2. vengono calcolati i costi del viaggio e applicati sconti
         
+    // prezzo della tratta per km
+        const kmPrice = 0.21
+        const ridePrice = userKm * kmPrice
+        const roundRidePrice = ridePrice.toFixed(2)
+
+        // sconto under e over
+        let totalUnderPrice;
+        let totalOverPrice;
+        let userMessage
+        
+        if(userAge < 18){
+            underDiscount = (ridePrice * 20) / 100;
+            totalUnderPrice = ridePrice - underDiscount;
+            userMessage = `Km da percorrere: ${userKm}Km. Prezzo standard per la tratta: ${roundRidePrice}€. Sconto minorenni del 20% applicato. Il prezzo effettivo del suo biglietto è: ${totalUnderPrice.toFixed(2)}€`
+        } else if(userAge >=65){
+            overDiscount = (ridePrice * 40) / 100;
+            totalOverPrice = ridePrice - overDiscount;
+            userMessage = `Km da percorrere: ${userKm}Km. Prezzo standard per la tratta: ${roundRidePrice}€. Sconto Over65 del 40% applicato. Il prezzo effettivo del suo biglietto è: ${totalOverPrice.toFixed(2)}€`
+        } else{
+            userMessage = `Km da percorrere: ${userKm}Km Il prezzo del suo biglietto è: ${roundRidePrice}€`
+        }
+
+        console.log(userMessage)
+     // 3. viene stampato a div #user-message il messaggio per l utente con prezzo e sconto applicato
+     const messageDiv = document.querySelector(`#user-message`) 
+     messageDiv.innerHTML = userMessage   
 })
     
-            // dati numerici convertiti da stringa a numero
-        // 2. vengono calcolati i costi del viaggio e applicati sconti
-        // 3. viene stampato a div #user-message il messaggio per l utente con prezzo e sconto applicato
+            
+        
+       
 
         
 // mettere bottone svuota campi in ascolto per evento al click
