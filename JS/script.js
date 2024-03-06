802e3b6b49a4602bf79bbda6ff0ef24965a705cf
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-03-06 16:42:14 +0100

    feat: add erase button function

--- JS/script.js
+++ JS/script.js
@@ -4,13 +4,12 @@ calcButton.addEventListener(`click`, function() {
     // al click:
     // 1. si trasformano i valori in variabili
         // dati numerici convertiti da stringa a numero
+        const userName = document.querySelector('#input-name').value;
         const userKm = parseInt(document.querySelector('#input-km').value);
         const userAge = parseInt(document.querySelector(`#input-age`).value);
-        console.log(userKm)
-        console.log(userAge)
+    
     // 2. vengono calcolati i costi del viaggio e applicati sconti
-        
-    // prezzo della tratta per km
+        // prezzo della tratta per km
         const kmPrice = 0.21
         const ridePrice = userKm * kmPrice
         const roundRidePrice = ridePrice.toFixed(2)
@@ -23,27 +22,31 @@ calcButton.addEventListener(`click`, function() {
         if(userAge < 18){
             underDiscount = (ridePrice * 20) / 100;
             totalUnderPrice = ridePrice - underDiscount;
-            userMessage = `Km da percorrere: ${userKm}Km. Prezzo standard per la tratta: ${roundRidePrice}€. Sconto minorenni del 20% applicato. Il prezzo effettivo del suo biglietto è: ${totalUnderPrice.toFixed(2)}€`
+            userMessage = `Salve ${userName}, Km da percorrere: ${userKm}Km. Prezzo standard per la tratta: ${roundRidePrice}€. Sconto minorenni del 20% applicato. Il prezzo effettivo del suo biglietto è: ${totalUnderPrice.toFixed(2)}€`
         } else if(userAge >=65){
             overDiscount = (ridePrice * 40) / 100;
             totalOverPrice = ridePrice - overDiscount;
-            userMessage = `Km da percorrere: ${userKm}Km. Prezzo standard per la tratta: ${roundRidePrice}€. Sconto Over65 del 40% applicato. Il prezzo effettivo del suo biglietto è: ${totalOverPrice.toFixed(2)}€`
+            userMessage = `Salve ${userName}, Km da percorrere: ${userKm}Km. Prezzo standard per la tratta: ${roundRidePrice}€. Sconto Over65 del 40% applicato. Il prezzo effettivo del suo biglietto è: ${totalOverPrice.toFixed(2)}€`
         } else{
-            userMessage = `Km da percorrere: ${userKm}Km Il prezzo del suo biglietto è: ${roundRidePrice}€`
+            userMessage = `Salve ${userName}, Km da percorrere: ${userKm}Km Il prezzo del suo biglietto è: ${roundRidePrice}€`
         }
 
-        console.log(userMessage)
      // 3. viene stampato a div #user-message il messaggio per l utente con prezzo e sconto applicato
      const messageDiv = document.querySelector(`#user-message`) 
      messageDiv.innerHTML = userMessage   
 })
-    
             
-        
-       
-
-        
 // mettere bottone svuota campi in ascolto per evento al click
+const eraseButton = document.querySelector(`#erase-button`);
+eraseButton.addEventListener(`click`, function(){
     // al click:
-        // 1. si cambia valore degli imput in spazio vuoto selezionando il contenuto
-        // 2. si cambia il valore del div con spazio vuoto
+    // 1. si cambia valore degli imput in spazio vuoto selezionando il contenuto
+    const userName = document.querySelector('#input-name').value = '';
+    const userKm = document.querySelector('#input-km').value = '';
+    const userAge = document.querySelector(`#input-age`).value = '';
+
+    // 2. si cambia il valore del div con spazio vuoto
+    const messageDiv = document.querySelector(`#user-message`) 
+    messageDiv.innerHTML = ''  
+})
+    
