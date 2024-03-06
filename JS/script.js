ab724f3ca098d0ba9e6e57b6d72b772a67b32978
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-03-06 16:03:26 +0100

    feat: add first calc button function

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,21 @@
-// chiedere numero di km da percorrere
-// chiedere età passeggero
-// calcolare sconto under e over
+// mettere bottone calcola in ascolto per evento al click
+const calcButton = document.querySelector(`#calc-button`);
+calcButton.addEventListener(`click`, function() {
+    // al click:
+        // 1. si trasformano i valori in variabili
+        const userKm = parseInt(document.querySelector('#input-km').value);
+        const userAge = parseInt(document.querySelector(`#input-age`).value);
+        console.log(userKm)
+        console.log(userAge)
+        
+})
+    
+            // dati numerici convertiti da stringa a numero
+        // 2. vengono calcolati i costi del viaggio e applicati sconti
+        // 3. viene stampato a div #user-message il messaggio per l utente con prezzo e sconto applicato
+
+        
+// mettere bottone svuota campi in ascolto per evento al click
+    // al click:
+        // 1. si cambia valore degli imput in spazio vuoto selezionando il contenuto
+        // 2. si cambia il valore del div con spazio vuoto
